a2acea4e95a1a52f5b7289129301fb9a4c9374f1
rakibctg112233 <rakibalhasan12121@gmail.com>
2026-04-14 16:51:04 +0400

    Replace require with cookie array for Facebook login
    Replaced the require statement with an array of cookie objects for Facebook login.

--- index.js
+++ index.js
@@ -1,1 +1,101 @@
-require("./Nayan/catalogs/Nayana.js")
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261585678772965%22%3A%22qAtRj3H3%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "datr",
+        "value": "GxDeafUZJ-dDcRXd5ButStJs",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "sb",
+        "value": "GxDeaXbKD7Otj0Udt6PMM-oM",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B832x424x1.7000000476837158",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61585678772965",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "xs",
+        "value": "45%3AvDIDnh4Ghm9gdg%3A2%3A1776160999%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "locale",
+        "value": "en_GB",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "pas",
+        "value": "61584866024929%3AKznUs4t0au%2C61585678772965%3A1M8cRyifAw",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "fr",
+        "value": "0AYa9UsxUC831sxBv.AWfHYllzdZAAjpYN9BgSQZkvsHEpi6aD5DqMy9w7UF_F-nViO0s.Bp3hAb..AAA.0.0.Bp3jdM.AWfmBxp82GozCIeLq-tvx_9cMws",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.130Z",
+        "lastAccessed": "2026-04-14T12:48:36.130Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "100727831%3BT%3A29602847",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.131Z",
+        "lastAccessed": "2026-04-14T12:48:36.131Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A3142%3Btimestamp%3A1776170828",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2026-04-14T12:48:36.131Z",
+        "lastAccessed": "2026-04-14T12:48:36.131Z"
+    }
+]
